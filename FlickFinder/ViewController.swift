c50960259889ef80c48896fba68c012daf0d1497
Ada Ji <adajixyz@gmail.com>
2015-11-06 23:37:18 +0800

    Create finder view and search request

--- FlickFinder/ViewController.swift
+++ FlickFinder/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  FlickFinder
+//  Flick Finder
 //
 //  Created by Ada Ji on 11/6/15.
 //  Copyright © 2015 Ada Ji. All rights reserved.
@@ -8,8 +8,84 @@
 
 import UIKit
 
+/* 1 - Define constants */
+let BASE_URL = "https://api.flickr.com/services/rest/"
+let METHOD_NAME = "flickr.galleries.getPhotos"
+let API_KEY = "ENTER_YOUR_API_KEY_HERE"
+let TEXT = "baby asian elephant"
+let SAFE_SEARCH = "1"
+let EXTRAS = "url_m"
+let DATA_FORMAT = "json"
+let NO_JSON_CALLBACK = "1"
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageLabel: UILabel!
+    @IBOutlet weak var phraseField: UITextField!
+    @IBOutlet weak var latitudeField: UITextField!
+    @IBOutlet weak var longitudeField: UITextField!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func searchByPhrase(sender: UIButton) {
+        getImageFromFlickr()
+    }
+    
+    func getImageFromFlickr() {
+        let methodArguments = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "text": TEXT,
+            "safe_search": SAFE_SEARCH,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(methodArguments)
+        let request = NSURLRequest(URL: NSURL(string: urlString)!)
+        
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            if let error = error {
+                print("Could not complete the request \(error)")
+            } else {
+                print(data)
+            }
+            
+//            guard (error == nil) else {
+//                print("There was an error with your request: \(error)")
+//                return
+//            }
+//            
+//            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+//                if let response = response as? NSHTTPURLResponse {
+//                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+//                } else if let response = response {
+//                    print("Your request returned an invalid response! Response: \(response)!")
+//                } else {
+//                    print("Your request returned an invalid response!")
+//                }
+//                return
+//            }
+//            
+//            guard let data = data else {
+//                print("No data was returned by the request!")
+//                return
+//            }
+//            
+//            print("data: \(data)")
+        }
+        
+        task.resume()
+    }
+    
+    @IBAction func searchByLatLon(sender: UIButton) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +96,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /* Helper function: Given a dictionary of parameters, convert to a string for a url */
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+            
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+    }
 
 }
 
