0d92efc63838fb4fb56fd2ac8c126b1b146b5f99
Ada Ji <adajixyz@gmail.com>
2015-11-08 15:34:40 +0800

    Complete phrase search request

--- FlickFinder/ViewController.swift
+++ FlickFinder/ViewController.swift
@@ -8,17 +8,19 @@
 
 import UIKit
 
-/* 1 - Define constants */
+// MARK: Globals
+
 let BASE_URL = "https://api.flickr.com/services/rest/"
-let METHOD_NAME = "flickr.galleries.getPhotos"
-let API_KEY = "ENTER_YOUR_API_KEY_HERE"
-let TEXT = "baby asian elephant"
+let METHOD_NAME = "flickr.photos.search"
+let API_KEY = "524445849254a06e0f74562c717c6f95"
 let SAFE_SEARCH = "1"
 let EXTRAS = "url_m"
 let DATA_FORMAT = "json"
 let NO_JSON_CALLBACK = "1"
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var imageLabel: UILabel!
@@ -27,15 +29,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var longitudeField: UITextField!
     @IBOutlet weak var titleLabel: UILabel!
     
+    var keyboardIsShowing: Bool!
+    
+    // MARK: Actions
+    
     @IBAction func searchByPhrase(sender: UIButton) {
+        searchImageByPhrase()
+    }
+    
+    func searchImageByPhrase() {
+        resetView()
+        imageLabel.text = "Searching..."
         getImageFromFlickr()
     }
+    
+    // MARK: Flickr API
     
     func getImageFromFlickr() {
         let methodArguments = [
             "method": METHOD_NAME,
             "api_key": API_KEY,
-            "text": TEXT,
+            "text": phraseField.text!,
             "safe_search": SAFE_SEARCH,
             "extras": EXTRAS,
             "format": DATA_FORMAT,
@@ -46,36 +60,51 @@ class ViewController: UIViewController {
         let urlString = BASE_URL + escapedParameters(methodArguments)
         let request = NSURLRequest(URL: NSURL(string: urlString)!)
         
-        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+        let task = session.dataTaskWithRequest(request) { (data, response, downloadError) in
             
-            if let error = error {
+            if let error = downloadError {
                 print("Could not complete the request \(error)")
             } else {
-                print(data)
+                let parsedResult: AnyObject!
+                do {
+                    parsedResult = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as! NSDictionary
+
+                    if let photosDictionary = parsedResult["photos"] as? [String: AnyObject] {
+                        var totalVar = 0
+                        if let total = photosDictionary["total"] as? String {
+                            totalVar = (total as NSString).integerValue
+                        }
+                        
+                        if totalVar > 0 {
+                            if let photosArray = photosDictionary["photo"] as? [[String: AnyObject]] {
+                                let index = Int(arc4random_uniform(UInt32(photosArray.count)))
+                                let photoDictionary = photosArray[index]
+                                let imageTitle = photoDictionary["title"] as? String // non-fatal
+                                if let imageUrlString = photoDictionary["url_m"] as? String {
+                                    let imageUrl = NSURL(string: imageUrlString)
+                                    if let imageData = NSData(contentsOfURL: imageUrl!) {
+                                        dispatch_async(dispatch_get_main_queue(), {
+                                            self.imageLabel.alpha = 0.0 // * Is this more efficient than ".hidden = true" ?
+                                            self.imageView.image = UIImage(data: imageData)
+                                            self.titleLabel.text = imageTitle ?? "(Untitled)"
+                                        })
+                                    }
+                                }
+                            }
+                        } else {
+                            dispatch_async(dispatch_get_main_queue(), {
+                                self.resetView()
+                                self.imageLabel.text = "No photos found. Search again."
+                            })
+                        }
+                    }
+                    
+                } catch {
+                    parsedResult = nil
+                    print("Could not parse the data as JSON: '\(data)'")
+                    return
+                }
             }
-            
-//            guard (error == nil) else {
-//                print("There was an error with your request: \(error)")
-//                return
-//            }
-//            
-//            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
-//                if let response = response as? NSHTTPURLResponse {
-//                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
-//                } else if let response = response {
-//                    print("Your request returned an invalid response! Response: \(response)!")
-//                } else {
-//                    print("Your request returned an invalid response!")
-//                }
-//                return
-//            }
-//            
-//            guard let data = data else {
-//                print("No data was returned by the request!")
-//                return
-//            }
-//            
-//            print("data: \(data)")
         }
         
         task.resume()
@@ -85,17 +114,121 @@ class ViewController: UIViewController {
         
     }
     
-    
+    // MARK: Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        keyboardIsShowing = false
+
+        phraseField.delegate = self
+        latitudeField.delegate = self
+        longitudeField.delegate = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        unsubscribeToKeyboardNotifications()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Text Field Delegate
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        addKeyboardDismissRecognizer()
+        
+        return true
     }
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == phraseField {
+            textField.resignFirstResponder()
+            
+            searchImageByPhrase()
+        }
+        
+        return true
+    }
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        removeKeyboardDismissRecognizer()
+        
+        return true
+    }
+    
+    // MARK: Show/Hide Keyboard
+    
+    // Adjust view frame when keyboard shows/hides
+    
+    func addKeyboardDismissRecognizer() {
+        let recognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+        view.addGestureRecognizer(recognizer)
+    }
+    
+    func removeKeyboardDismissRecognizer() {
+        //        view.removeGestureRecognizer(recognizer)
+    }
+    
+    func handleSingleTap(recognizer: UITapGestureRecognizer) {
+        if phraseField.isFirstResponder() {
+            phraseField.resignFirstResponder()
+        }
+        else if latitudeField.isFirstResponder() {
+            latitudeField.resignFirstResponder()
+        }
+        else if longitudeField.isFirstResponder() {
+            longitudeField.resignFirstResponder()
+        }
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if !keyboardIsShowing! {
+            moveViewVertically(getKeyboardHeight(notification), up: true)
+            keyboardIsShowing = true
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if keyboardIsShowing! {
+            moveViewVertically(getKeyboardHeight(notification), up: false)
+            keyboardIsShowing = false
+        }
+    }
+    
+    func moveViewVertically(distance: CGFloat, up: Bool) {
+        let dist = distance * (up ? -1 : 1)
+        view.frame.origin.y += dist
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+    }
+    
+    // MARK: Helper Functions
+    
+    func resetView() {
+        imageView.image = nil
+        imageLabel.alpha = 1.0
+        titleLabel.text = ""
+    }
+    
     /* Helper function: Given a dictionary of parameters, convert to a string for a url */
     func escapedParameters(parameters: [String : AnyObject]) -> String {
         
@@ -116,6 +249,6 @@ class ViewController: UIViewController {
         
         return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
     }
-
+    
 }
 
